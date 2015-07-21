edf062db836d27b766a8cc51afd420d88c8f78cb
nabarunnag <nag@cs.wisc.edu>
2015-07-20 23:08:26 -0700

    Modifying the code to implement asynchronous method of doing an HTTP request

--- app/src/main/java/com/riben/nabarunnag/stormy/MainActivity.java
+++ app/src/main/java/com/riben/nabarunnag/stormy/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -33,15 +34,25 @@ public class MainActivity extends ActionBarActivity {
                 .build();
 
         Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-               Log.v(TAG,response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG,"Error while getting the response from forecast server : ",e);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
 
-        }
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()){
+                        Log.v(TAG,response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG,"Error while getting the response from forecast server : ",e);
+
+                }
+            }
+        });
+
     }
 
 
